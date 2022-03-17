488b73caf43427b6fdc6f88de675b72ed1f348e7
Neha <nehasijaria@gmail.com>
2022-03-17 01:04:33 -0400

    add req params

--- index.js
+++ index.js
@@ -7,10 +7,31 @@ const app = express();//app object created
 app.get('/', (req,res)=>{
   res.send('hello');
 });
-//this route return list of courses
+//this route return list of all courses
 app.get("/api/courses", (req, res) => {
   res.send([1,2,3,4,5]);
 });
+//this route return single courses(id of course)
+app.get("/api/courses/:id", (req, res) => {
+  console.log(req.params.id);
+  res.send(req.params.id);
+});
+//multiple route paramas
+app.get("/api/courses/:stack/:name", (req, res) => {
+  //console.log(req.params);
+  res.send(req.params);
+});
+//{"frontend":"frontend","java":"java"}
+//
+app.get("/api/courses/:stack/:name", (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+
+
+
+
 //port dynamically assign  on deployment: not 4500
 const port = process.env.PORT || 4500;
 app.listen(port, ()=>{
