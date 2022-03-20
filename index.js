423a9ebb714f1474ee4d7e0c31ae1a87e9e5aa0c
Neha <nehasijaria@gmail.com>
2022-03-20 00:08:04 -0400

    use template eng to render view

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const config = require('./config')
+//const config = require('./config')
 const morgan = require('morgan');
 const helmet = require("helmet");
 const bodyParser = require("body-parser");
 const Joi = require('joi'); //return class
 const startupDebugger = require('debug')('app:startup')
 const dbDebug = require("debug")("app:db");
-
-const logger = require('./logger');
-const authenticate = require('./authetication');
-//require('express')//return fn stored in var express
+const logger = require("./logger");
 const express = require("express");
 const app = express();//app object created
+
+//template engine
+app.set('view engine', 'pug');
+//app.set('views', './views');
+
+const authenticate = require('./authetication');
+//require('express')//return fn stored in var express
+
 //move to separate module
 // app.use(function(req,res,next){
 //   console.log('logging');
@@ -31,7 +36,8 @@ const courses = [
 //whn '/' hits, this callbk run and send to browser
 
 app.get('/', (req,res)=>{
-  res.send('hello');
+  //res.send('hello');
+  res.render('index', { title: 'exp-app', msg: 'hello' })
 });
 //this route return list of all courses
 app.get("/api/courses", (req, res) => {
